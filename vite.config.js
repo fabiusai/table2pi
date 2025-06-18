b1208cc2c0c9b31405b404e74f2958053dbb5288
fabiusai <fabio.aita@gmail.com>
2025-06-18 12:52:36 +0200

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  // ⚠️  Sostituisci <REPO> con il nome del tuo repository quando deployi su GitHub Pages
-  base: process.env.NODE_ENV === 'production' ? '/table2pi/' : '/'
-});
