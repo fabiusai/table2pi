88f32e1e5b37a3ba127d665967db8682b4b0240d
fabiusai <fabio.aita@gmail.com>
2025-06-18 12:44:37 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   // ⚠️  Sostituisci <REPO> con il nome del tuo repository quando deployi su GitHub Pages
-  base: process.env.NODE_ENV === 'production' ? '/<REPO>/' : '/'
+  base: process.env.NODE_ENV === 'production' ? '/table2pi/' : '/'
 });
